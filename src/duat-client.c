37d7bb2a55c8c58b98f0685d7324dea647c81025
Magnus Deininger <dma05@web.de>
2009-03-01 18:15:24 +0000

    connections seem to get established, but thats about it

--- src/duat-client.c
+++ src/duat-client.c
@@ -31,16 +31,57 @@
 #include <curie/multiplex.h>
 #include <duat/9p-client.h>
 
-struct sexpr_io *stdio;
+enum op
+{
+    op_nop,
+    op_cat,
+    op_ls,
+    op_write
+};
+
+static char *i_path           = (char *)0;
+static enum op i_op           = op_nop;
+static struct io *stdout      = (struct io *)0;
+static struct sexpr_io *stdio = (struct sexpr_io *)0;
 
 static int print_help ()
 {
     return 1;
 }
 
+static void on_read (struct io *io, void *aux)
+{
+    io_write (stdout, io->buffer + io->position, io->length - io->position);
+    io->position = io->length;
+}
+
+static void on_close (struct io *io, void *aux)
+{
+    io_close (stdout);
+    cexit(0);
+}
+
 static void on_connect (struct d9r_io *io, void *aux)
 {
-    cexit (0);
+    struct io *n;
+
+    sx_write (stdio, make_symbol ("connected"));
+
+    switch (i_op)
+    {
+        case op_cat:
+            n = io_open_read_9p (io, i_path);
+            break;
+//        case op_write:
+/*            n = io_open_write_9p (io, i_path);*/
+//            break;
+//        case op_ls:
+//            break;
+        default:
+            cexit (4);
+    }
+
+    multiplex_add_io (n, on_read, on_close, (void *)0);
 }
 
 static void on_error (struct d9r_io *io, const char *error, void *aux)
@@ -55,9 +96,13 @@ int cmain()
     sexpr i_socket = sx_false;
     stdio = sx_open_stdio();
 
+    stdout = io_open (1);
+    stdout->type = iot_write;
+
+    multiplex_io ();
     multiplex_d9c ();
 
-    for (int i = 0; curie_argv[i] != (char *)0; i++)
+    for (int i = 1; curie_argv[i] != (char *)0; i++)
     {
         if (curie_argv[i][0] == '-')
         {
@@ -80,14 +125,63 @@ int cmain()
                 }
             }
 
-            i = xn;
+            i = xn - 1;
+        }
+        else if (i_op == op_nop)
+        {
+            char *op = curie_argv[i];
+
+            switch (op[0])
+            {
+                case 'l':
+                    if ((op[1] == 's') && (op[2] == 0))
+                    {
+                        i_op = op_ls;
+                        break;
+                    }
+                    return 10;
+                case 'c':
+                    if ((op[1] == 'a') && (op[2] == 't') && (op[3] == 0))
+                    {
+                        i_op = op_cat;
+                        break;
+                    }
+                    return 11;
+                case 'r':
+                    if ((op[1] == 'e') && (op[2] == 'a') && (op[3] == 'd') &&
+                         (op[4] == 0))
+                    {
+                        i_op = op_cat;
+                        break;
+                    }
+                    return 12;
+                case 'w':
+                    if ((op[1] == 'r') && (op[2] == 'i') && (op[3] == 't') &&
+                        (op[4] == 'e') && (op[5] == 0))
+                    {
+                        i_op = op_write;
+                        break;
+                    }
+                    return 13;
+                default:
+                    return 14;
+            }
+        }
+        else if (i_path == (char *)0)
+        {
+            i_path = curie_argv[i];
         }
     }
 
-    if (!stringp(i_socket)) return 2;
+    if (!stringp(i_socket) || (i_op == op_nop) || (i_path == (char *)0))
+    {
+        return 2;
+    }
 
     multiplex_add_d9c_socket
             (sx_string (i_socket), on_connect, on_error, (void *)0);
+    multiplex_add_io_no_callback (stdout);
+    multiplex_add_sexpr (stdio, (void *)0, (void *)0);
 
     while (multiplex() == mx_ok);
 
